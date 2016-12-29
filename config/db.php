3ddf1c8a9e586ce24f3c0df950043d77ab4d6a1d
neilxu <mr.neilxu@gmail.com>
2016-12-29 14:11:14 +0800

    begin project

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+	'class'    => 'yii\db\Connection',
+	'dsn'      => 'mysql:host=www.sues.club;dbname=clothes',
+	'username' => 'root',
+	'password' => 'root',
+	'charset'  => 'utf8',
+];
