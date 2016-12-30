b05c734abdc3d0c56cd037a646de4b11459512e9
neilxu <mr.neilxu@gmail.com>
2016-12-30 14:20:02 +0800

    add

--- views/site/entry.php
+++ views/site/entry.php
@@ -4,7 +4,24 @@
 $this->title = '规则录入';
 ?>
 <div class="site-index">
-    
-<h1>哈哈~</h1>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-6">
+                <form action="" align="center">表单一</form>
+            </div>
+            <div class="col-xs-6">
+                <form action="" align="center">表单二</form>
+            </div>
+        </div>
+        <div class="row" >
+            <div class="col-xs-6">
+                <form action="" align="center">表单三</form>
+            </div>
+            <div class="col-xs-6">
+                <form action="" align="center">表单四</form>
+            </div>
+        </div>
+    </div>
+
 
 </div>
